2cd0f9f545a62ade53b1180da61c152861cb478f
Tomas Sardyha <darsain@gmail.com>
2013-02-08 11:41:14 +0100

    Fix end limit on forceCentered navigation without items, closes #28

--- src/sly.js
+++ src/sly.js
@@ -193,7 +193,7 @@
 
 				// Set limits
 				pos.start = centerOffset;
-				pos.end   = forceCenteredNav ? items[items.length - 1].center : Math.max(slideeSize - frameSize, 0);
+				pos.end   = forceCenteredNav ? (items.length ? items[items.length - 1].center : centerOffset) : Math.max(slideeSize - frameSize, 0);
 
 				// Fix overflowing activeItem
 				if (rel.activeItem >= items.length) {
