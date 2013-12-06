542c97029f31b33e6a7140515c5c3a946789b15f
Tomas Sardyha <darsain@gmail.com>
2013-12-06 14:53:09 +0100

    Fixing meta repo url

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,13 +11,13 @@ module.exports = function (grunt) {
 		meta: {
 			banner: '/*!\n' +
 				' * <%= pkg.name %> <%= pkg.version %> - <%= grunt.template.today("dS mmm yyyy") %>\n' +
-				' * <%= pkg.homepage %>\n' +
+				' * <%= pkg.repository.homepage %>\n' +
 				' *\n' +
 				' * Licensed under the <%= pkg.licenses[0].type %> license.\n' +
 				' * <%= pkg.licenses[0].url %>\n' +
 				' */\n',
 			bannerLight: '/*! <%= pkg.name %> <%= pkg.version %>' +
-				' - <%= grunt.template.today("dS mmm yyyy") %> | <%= pkg.homepage %> */'
+				' - <%= grunt.template.today("dS mmm yyyy") %> | <%= pkg.repository.homepage %> */'
 		},
 
 		// JSHint the code.
