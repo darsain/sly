99d8820f3f602f62b993b9b64ef3eee5946e8b01
Tomas Sardyha <darsain@gmail.com>
2014-11-30 12:38:53 +0100

    Make default boolean options boolean, not integers.

--- src/sly.js
+++ src/sly.js
@@ -2082,14 +2082,14 @@
 
 	// Default options
 	Sly.defaults = {
-		horizontal: 0, // Switch to horizontal mode.
+		horizontal: false, // Switch to horizontal mode.
 
 		// Item based navigation
-		itemNav:      null, // Item navigation type. Can be: 'basic', 'centered', 'forceCentered'.
-		itemSelector: null, // Select only items that match this selector.
-		smart:        0,    // Repositions the activated item to help with further navigation.
-		activateOn:   null, // Activate an item on this event. Can be: 'click', 'mouseenter', ...
-		activateMiddle: 0,  // Always activate the item in the middle of the FRAME. forceCentered only.
+		itemNav:        null,  // Item navigation type. Can be: 'basic', 'centered', 'forceCentered'.
+		itemSelector:   null,  // Select only items that match this selector.
+		smart:          false, // Repositions the activated item to help with further navigation.
+		activateOn:     null,  // Activate an item on this event. Can be: 'click', 'mouseenter', ...
+		activateMiddle: false, // Always activate the item in the middle of the FRAME. forceCentered only.
 
 		// Scrolling
 		scrollSource: null, // Element for catching the mouse wheel scrolling. Default is FRAME.
@@ -2097,21 +2097,21 @@
 		scrollHijack: 300,  // Milliseconds since last wheel event after which it is acceptable to hijack global scroll.
 
 		// Dragging
-		dragSource:    null, // Selector or DOM element for catching dragging events. Default is FRAME.
-		mouseDragging: 0,    // Enable navigation by dragging the SLIDEE with mouse cursor.
-		touchDragging: 0,    // Enable navigation by dragging the SLIDEE with touch events.
-		releaseSwing:  0,    // Ease out on dragging swing release.
-		swingSpeed:    0.2,  // Swing synchronization speed, where: 1 = instant, 0 = infinite.
-		elasticBounds: 0,    // Stretch SLIDEE position limits when dragging past FRAME boundaries.
-		interactive:   null, // Selector for special interactive elements.
+		dragSource:    null,  // Selector or DOM element for catching dragging events. Default is FRAME.
+		mouseDragging: false, // Enable navigation by dragging the SLIDEE with mouse cursor.
+		touchDragging: false, // Enable navigation by dragging the SLIDEE with touch events.
+		releaseSwing:  false, // Ease out on dragging swing release.
+		swingSpeed:    0.2,   // Swing synchronization speed, where: 1 = instant, 0 = infinite.
+		elasticBounds: false, // Stretch SLIDEE position limits when dragging past FRAME boundaries.
+		interactive:   null,  // Selector for special interactive elements.
 
 		// Scrollbar
-		scrollBar:     null, // Selector or DOM element for scrollbar container.
-		dragHandle:    0,    // Whether the scrollbar handle should be draggable.
-		dynamicHandle: 0,    // Scrollbar handle represents the ratio between hidden and visible content.
-		minHandleSize: 50,   // Minimal height or width (depends on sly direction) of a handle in pixels.
-		clickBar:      0,    // Enable navigation by clicking on scrollbar.
-		syncSpeed:     0.5,  // Handle => SLIDEE synchronization speed, where: 1 = instant, 0 = infinite.
+		scrollBar:     null,  // Selector or DOM element for scrollbar container.
+		dragHandle:    false, // Whether the scrollbar handle should be draggable.
+		dynamicHandle: false, // Scrollbar handle represents the ratio between hidden and visible content.
+		minHandleSize: 50,    // Minimal height or width (depends on sly direction) of a handle in pixels.
+		clickBar:      false, // Enable navigation by clicking on scrollbar.
+		syncSpeed:     0.5,   // Handle => SLIDEE synchronization speed, where: 1 = instant, 0 = infinite.
 
 		// Pagesbar
 		pagesBar:       null, // Selector or DOM element for pages bar container.
@@ -2130,10 +2130,10 @@
 		nextPage: null, // Selector or DOM element for "next page" button.
 
 		// Automated cycling
-		cycleBy:       null, // Enable automatic cycling by 'items' or 'pages'.
-		cycleInterval: 5000, // Delay between cycles in milliseconds.
-		pauseOnHover:  0,    // Pause cycling when mouse hovers over the FRAME.
-		startPaused:   0,    // Whether to start in paused sate.
+		cycleBy:       null,  // Enable automatic cycling by 'items' or 'pages'.
+		cycleInterval: 5000,  // Delay between cycles in milliseconds.
+		pauseOnHover:  false, // Pause cycling when mouse hovers over the FRAME.
+		startPaused:   false, // Whether to start in paused sate.
 
 		// Mixed options
 		moveBy:        300,     // Speed in pixels per second used by forward and backward buttons.
@@ -2143,8 +2143,8 @@
 		keyboardNavBy: null,    // Enable keyboard navigation by 'items' or 'pages'.
 
 		// Classes
-		draggedClass:  'dragged',  // Class for dragged elements (like SLIDEE or scrollbar handle).
-		activeClass:   'active',   // Class for active items and pages.
-		disabledClass: 'disabled'  // Class for disabled navigation elements.
+		draggedClass:  'dragged', // Class for dragged elements (like SLIDEE or scrollbar handle).
+		activeClass:   'active',  // Class for active items and pages.
+		disabledClass: 'disabled' // Class for disabled navigation elements.
 	};
 }(jQuery, window));
